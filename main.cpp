21f1f123895265aea2bc87a774986285f7cdfc14
evsasse <evsasse@gmail.com>
2017-08-10 10:54:34 -0300

    getting ui from glade file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,62 @@
-#include "helloworld.hpp"
-#include <gtkmm/application.h>
+#include <gtkmm.h>
+#include <iostream>
 
-int main (int argc, char *argv[])
+Gtk::Window* pHelloWorldWindow = nullptr;
+// Gtk::Window* pAnotherWindow = nullptr;
+
+static
+void on_button_clicked()
 {
-  auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+  if(pHelloWorldWindow){
+  	std::cout << "Bye World!\n";
+    pHelloWorldWindow->hide(); //hide() will cause main::run() to end.
+  }
+}
 
-  HelloWorld helloworld;
+int main (int argc, char **argv)
+{
+  auto app = Gtk::Application::create(argc, argv, "br.ufsc.ine5420.sgi");
 
-  //Shows the window and returns when it is closed.
-  return app->run(helloworld);
+  //Load the GtkBuilder file and instantiate its widgets:
+  auto refBuilder = Gtk::Builder::create();
+  try
+  {
+    refBuilder->add_from_file("helloworld.glade");
+  }
+  catch(const Glib::FileError& ex)
+  {
+    std::cerr << "FileError: " << ex.what() << std::endl;
+    return 1;
+  }
+  catch(const Glib::MarkupError& ex)
+  {
+    std::cerr << "MarkupError: " << ex.what() << std::endl;
+    return 1;
+  }
+  catch(const Gtk::BuilderError& ex)
+  {
+    std::cerr << "BuilderError: " << ex.what() << std::endl;
+    return 1;
+  }
+
+  //Get the GtkBuilder-instantiated Window:
+  refBuilder->get_widget("HelloWorldWindow", pHelloWorldWindow);
+  if(pHelloWorldWindow)
+  {
+    //Get the GtkBuilder-instantiated Button, and connect a signal handler:
+    Gtk::Button* pButton = nullptr;
+    refBuilder->get_widget("button1", pButton);
+    if(pButton)
+    {
+      pButton->signal_clicked().connect( sigc::ptr_fun(on_button_clicked) );
+    }
+
+    std::cout << "Hello World!\n";
+
+    app->run(*pHelloWorldWindow);
+  }
+
+  delete pHelloWorldWindow;
+
+  return 0;
 }
